a9524dce99fe556c2e2fb88427af6316994e9e5f
Carl Molnar <cpmolnar@gmail.com>
2019-08-29 15:46:31 -0500

    Fixed inception_block_factorized, added the auxiliary classifier, and introduced it in the loss function

--- inception_v2.py
+++ inception_v2.py
@@ -27,6 +27,8 @@ class inception_v2:
         self.inception3_2 = self.inception_block_spacial_aggregation(self.inception3_1, 128, 128, 192, 32, 96, 64)
         self.pool3 = self.max_pool(self.inception3_2)
 
+        self.ac3 = self.auxiliary_classifier(self.pool3, num_1x1=128, num_outputs=10)
+
         self.inception4_1 = self.inception_block_factorized(self.pool3, 192, 96, 208, 16, 48, 64)
         self.inception4_2 = self.inception_block_factorized(self.inception4_1, 160, 112, 224, 24, 64, 64)
         self.inception4_3 = self.inception_block_factorized(self.inception4_2, 128, 128, 256, 24, 64, 64)
@@ -74,18 +76,18 @@ class inception_v2:
 
         return concat
 
-    def inception_block_factorized(self, val, num_1x1, num_3x3_reduce, num_3x3, num_5x5_reduce, num_5x5, pool_proj):
+    def inception_block_factorized(self, val, num_1x1, num_nxn_single_reduce, num_nxn, num_nxn_double_reduce, num_nxn_double, pool_proj, n=3):
         branch_0 = self.conv_layer(val, num_outputs=num_1x1, kernel_size=1, stride=1, padding='SAME')
         
-        branch_1 = self.conv_layer(val, num_outputs=num_3x3_reduce, kernel_size=1, stride=1, padding='SAME')
-        branch_1 = self.conv_layer(branch_1, num_outputs=num_3x3, kernel_size=[1, 3], stride=1, padding='SAME')
-        branch_1 = self.conv_layer(branch_1, num_outputs=num_3x3, kernel_size=[3, 1], stride=1, padding='SAME')
+        branch_1 = self.conv_layer(val, num_outputs=num_nxn_single_reduce, kernel_size=1, stride=1, padding='SAME')
+        branch_1 = self.conv_layer(branch_1, num_outputs=num_nxn, kernel_size=[1, n], stride=1, padding='SAME')
+        branch_1 = self.conv_layer(branch_1, num_outputs=num_nxn, kernel_size=[n, 1], stride=1, padding='SAME')
 
-        branch_2 = self.conv_layer(val, num_outputs=num_5x5_reduce, kernel_size=1, stride=1, padding='SAME')
-        branch_2 = self.conv_layer(branch_2, num_outputs=num_5x5, kernel_size=[1, 3], stride=1, padding='SAME')
-        branch_2 = self.conv_layer(branch_2, num_outputs=num_5x5, kernel_size=[3, 1], stride=1, padding='SAME')
-        branch_2 = self.conv_layer(branch_2, num_outputs=num_5x5, kernel_size=[1, 3], stride=1, padding='SAME')
-        branch_2 = self.conv_layer(branch_2, num_outputs=num_5x5, kernel_size=[3, 1], stride=1, padding='SAME')
+        branch_2 = self.conv_layer(val, num_outputs=num_nxn_double_reduce, kernel_size=1, stride=1, padding='SAME')
+        branch_2 = self.conv_layer(branch_2, num_outputs=num_nxn_double, kernel_size=[1, n], stride=1, padding='SAME')
+        branch_2 = self.conv_layer(branch_2, num_outputs=num_nxn_double, kernel_size=[n, 1], stride=1, padding='SAME')
+        branch_2 = self.conv_layer(branch_2, num_outputs=num_nxn_double, kernel_size=[1, n], stride=1, padding='SAME')
+        branch_2 = self.conv_layer(branch_2, num_outputs=num_nxn_double, kernel_size=[n, 1], stride=1, padding='SAME')
 
         branch_3 = self.max_pool(val, stride=1)
         branch_3 = self.conv_layer(branch_3, num_outputs=pool_proj, kernel_size=1, stride=1, padding='SAME')
@@ -113,6 +115,13 @@ class inception_v2:
 
         return concat
 
+    def auxiliary_classifier(self, val, num_1x1, num_outputs):
+        ac = self.avg_pool(val, kernel_size=5, stride=3)
+        ac = tf.contrib.layers.conv2d(ac, num_outputs=num_1x1, kernel_size=1, weights_initializer=tf.contrib.layers.xavier_initializer(seed = 1))
+        ac = self.fc_layer(ac, num_outputs=10)
+
+        return ac
+
 def circular_learning_rate(global_step, min_lr=0.0001, max_lr=0.0008, step_size=2000, gamma=0.99994, mode='triangular2', name=None):
     # Circular learning rate formula is from [https://arxiv.org/pdf/1506.01186.pdf]
     cycle = tf.floor(1. + tf.cast(global_step, "float32") / (2. * step_size))
@@ -136,20 +145,25 @@ def model(X_train, X_val, y_train, y_val, num_epochs=20, print_cost = True, mini
 
     model = inception_v2()
     model.build(input)
-    output = model.fc6
-    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=output, labels=labels))
+    logits = model.fc6
+    aux_logits = model.ac3
+    tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits, label_smoothing=0.1, weights=1.0)
+    tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=aux_logits, label_smoothing=0.1, weights=0.4)
+
+    losses = tf.get_collection(tf.GraphKeys.LOSSES)
+    loss_op = tf.reduce_mean(tf.add_n(losses))
     optimizer = tf.train.AdamOptimizer(learning_rate=circular_learning_rate(global_step, min_lr, max_lr, step_size))
     train_op = optimizer.minimize(loss_op, global_step=global_step)
-    acc, acc_op = tf.metrics.accuracy(labels=tf.argmax(labels, 1), predictions=tf.argmax(output,1))
+    acc, acc_op = tf.metrics.accuracy(labels=tf.argmax(labels, 1), predictions=tf.argmax(logits,1))
 
     def compute_accuracy(X, Y, mini_batch_size=MINI_BATCH_SIZE):
-            accuracy=[]
-            num_minibatches = int(X.shape[0] / mini_batch_size)
-            minibatches = random_mini_batches(X, Y, mini_batch_size)
-            for minibatch in minibatches:
-                (minibatch_X, minibatch_Y) = minibatch
-                accuracy.append(sess.run(acc_op, {input:minibatch_X, labels: minibatch_Y}))
-            return np.mean(accuracy)
+        accuracy=[]
+        num_minibatches = int(X.shape[0] / mini_batch_size)
+        minibatches = random_mini_batches(X, Y, mini_batch_size)
+        for minibatch in minibatches:
+            (minibatch_X, minibatch_Y) = minibatch
+            accuracy.append(sess.run(acc_op, {input:minibatch_X, labels: minibatch_Y}))
+        return np.mean(accuracy)
 
     print("Trainable variables:", np.sum([np.prod(var.shape) for var in tf.trainable_variables()]))
 
